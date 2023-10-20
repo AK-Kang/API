1e91cd2cb70d93f87549cf8ee080b33cfe18c428
Anni Kang <62228243+AK-Kang@users.noreply.github.com>
2023-10-20 13:03:25 -0400

    Add file to calculate age and day of the week in R

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
+
 from flask import Flask, request, jsonify
-from flask_cors import CORS 
 import rpy2.robjects as robjects
-import pdfkit
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/api/send', methods=['POST'])
-def send_email():
-    data = request.json
-    
+@app.route("/calculate-age", methods=["POST"])
+
+def calculate_age():
+    data = request.get_json()
     # apply R functions to get age and day of week
     r_func = robjects.r("""
         function(dob) {
@@ -23,54 +16,10 @@ def send_email():
             return(list(age = age, day_of_week = day_of_week))
         }
     """)
-    
     result = r_func(data['dob'])
     age = result.rx('age')[0][0]
     day_of_week = result.rx('day_of_week')[0][0]
-    
-    # generate PDF using data
+    return jsonify({"age": age, "day of week": day_of_week})
 
-    pdf_content = f"""
-    <h1>Details</h1>
-    <p>Name: {data['firstName']} {data['lastName']}</p>
-    <p>Age: {age}</p>
-    <p>Day of the Week of Birth: {day_of_week}</p>
-    """
-
-    pdf_path = "output.pdf"
-    pdfkit.from_string(pdf_content, pdf_path)
-
-    # Email pdf
-    sender_email = "akang620@gmail.com"
-    password = "445182694"  
-    receiver_email = data['email']
-
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = "PDF"
-
-    with open(pdf_path, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-        encoders.encode_base64(part)
-        part.add_header(
-            "Content-Disposition",
-            f"attachment; filename= {pdf_path}",
-        )
-        message.attach(part)
-
-    text = message.as_string()
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server: # Using Gmail SMTP for this example
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, text)
-
-    return jsonify({
-        'age': age,
-        'day_of_week': day_of_week,
-        'message': 'Email sent successfully'
-    })
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
